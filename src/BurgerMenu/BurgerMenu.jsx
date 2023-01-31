d073dd4c6d946796f7cb8f61a782db9e00de65d2
IvailoManolov <ivomoreras@abv.bg>
2023-01-31 18:02:15 +0200

    burger-menu-animation-logic

--- src/BurgerMenu/BurgerMenu.jsx
+++ src/BurgerMenu/BurgerMenu.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 import "./BurgerMenu.css"
 
 const BurgerMenu = () => {
+
+  const[isOpen,setIsOpen] = useState(false)
+
+  const setOpenedState = () => {
+    let reverseOpened = isOpen
+
+    setIsOpen(!reverseOpened)
+  }
+
   return (
-    <div className='menu-button'>
-        <div className="menu-button-burger">
+    <div className= {isOpen ? 'menu-button' : 'menu-button-open'} onClick={() => setOpenedState()}>
+        <div className="menu-button-burger" >
         </div>
     </div>
   )
