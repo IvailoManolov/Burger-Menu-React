05fcd33aa7aa1e1baec33684bc45e33104557193
IvailoManolov <ivomoreras@abv.bg>
2023-01-31 17:32:56 +0200

    Burger-Menu-Initialization

---
+++ src/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./BurgerMenu.css"
+
+const BurgerMenu = () => {
+  return (
+    <div className='menu-button'>
+        <div className="menu-button-burger">
+        </div>
+    </div>
+  )
+}
+
+export default BurgerMenu
